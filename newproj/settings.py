60de13aaeef287804b434b92975a809f9cf18116
Your Name <you@example.com>
2017-07-18 18:31:10 +0530

    Add vednor UI changes working

--- newproj/settings.py
+++ newproj/settings.py
@@ -110,24 +110,24 @@ WSGI_APPLICATION = 'newproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
-#   'default': {
-#       'ENGINE': 'django.db.backends.mysql',
-#       'NAME': 'drowse_db',
-#       'HOST': '/var/lib/mysql/mysql.sock',
-#       'PORT': '3306',
-#       'USER': 'newuser',
-#       'PASSWORD': 'rootDB123'
-#   }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+
+DATABASES = {
+  'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': 'drowse_db',
+      'HOST': '/var/lib/mysql/mysql.sock',
+      'PORT': '3306',
+      'USER': 'newuser',
+      'PASSWORD': 'rootDB123'
+  }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
